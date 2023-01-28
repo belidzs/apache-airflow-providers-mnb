99f43f6a981aac5fbd3670f30e926c5db561a892
Balázs Keresztury <b@belidzs.hu>
2023-01-28 21:00:46 +0000

    Bump version

--- airflow/providers/mnb/__init__.py
+++ airflow/providers/mnb/__init__.py
@@ -3,5 +3,5 @@ def get_provider_info():
         "package-name": "apache-airflow-providers-mnb", # Required
         "name": "Exchange rates by MNB (Hungarian National Bank)", # Required
         "description": "Exchange rates by MNB (Hungarian National Bank).", # Required
-        "versions": ["1.0.1"] # Required
+        "versions": ["1.0.2"] # Required
     }
